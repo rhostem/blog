4c02c23729c6a10649b4e31f1fbd420f12bf210d
rhostem <syoung.j@gmail.com>
2020-03-07 23:08:56 +0900

    빌드 결과에 소스 맵 제외, 배포 후 예전 파일은 제거 과정 추가

--- gatsby-node.js
+++ gatsby-node.js
@@ -116,3 +116,13 @@ exports.createPages = ({ graphql, actions }) => {
     }
   })
 }
+
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // If production JavaScript and CSS build
+  if (stage === 'build-javascript') {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+}
